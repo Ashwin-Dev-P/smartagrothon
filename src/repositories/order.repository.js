e42d78ea801fe4b1b272af7aab93c2d67f7abe77
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-02-23 01:53:17 +0530

    farmer order history and details

--- src/repositories/order.repository.js
+++ src/repositories/order.repository.js
@@ -42,6 +42,7 @@ const getOrderHistoryForFarmerRepository = async (farmer_id, status) => {
 		//.populate("farmer_ids", "address phone_number location username email")
 		//.populate("consumer_id", "address phone_number location username email")
 		.populate("product_ids", "name price image quantity")
+		.sort("status -createdAt")
 		.lean();
 
 	return order_history;
